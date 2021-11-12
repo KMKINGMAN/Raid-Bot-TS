17b09dba612168934f2864936e57b8fcee747893
KMKINGMAN <84540534+KMKINGMAN@users.noreply.github.com>
2021-11-12 13:43:23 +0300

    Fix Config.ts
    just change old content from 
    ```js
    import dotenv from 'dotenv'
    dotenv.config();
    import * as CONFIG from '../config.json'
    import { ConfigData } from './types/KING-NUKE'
    export default {
        token: process.env['token']|| CONFIG.basic.token ,
        prefix: process.env['prerfix']|| CONFIG.basic.prefix,
        devs: CONFIG.WhiteListe
    } as ConfigData
    ```
    to 
    ```js
    import dotenv from 'dotenv'
    dotenv.config();
    import * as CONFIG from '../config.json'
    import { ConfigData } from './types/KING-NUKE'
    export default {
        token: process.env['token'] ? process.env['token'] : CONFIG.basic.token ,
        prefix: process.env['prerfix'] ? process.env['prerfix'] : CONFIG.basic.prefix,
        devs: CONFIG.WhiteListe
    } as ConfigData
    ```

--- KINGMAN_NUKE/Config.ts
+++ KINGMAN_NUKE/Config.ts
@@ -3,7 +3,7 @@ dotenv.config();
 import * as CONFIG from '../config.json'
 import { ConfigData } from './types/KING-NUKE'
 export default {
-    token: process.env['token']|| CONFIG.basic.token ,
-    prefix: process.env['prerfix']|| CONFIG.basic.prefix,
+    token: process.env['token'] ? process.env['token'] : CONFIG.basic.token ,
+    prefix: process.env['prerfix'] ? process.env['prerfix'] : CONFIG.basic.prefix,
     devs: CONFIG.WhiteListe
 } as ConfigData
